9d613b578d8b91bb0bf9d81981879bacf9e759c0
TousifNuhan <mubtasimtousif@gmail.com>
2024-02-07 23:34:51 +0600

    Completed

---
+++ problem1.js
@@ -0,0 +1,11 @@
+function calculateMoney(ticketSale) {
+     if(ticketSale>=0){
+               ticketCost=ticketSale*120;
+               othersCost=500+(8*50);
+               return ticketCost-othersCost;
+     }
+     return "Please give an accurate number!!!";
+    
+    }
+
+const ticket=calculateMoney(10);
